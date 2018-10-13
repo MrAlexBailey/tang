fa14e59d54470fe6e64c787d31c43837e8961174
Alex Bailey <mralexbailey@gmail.com>
2018-10-13 13:23:07 -0400

    Subpage and CSS setup

--- website/views.py
+++ website/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'website/home.html')
+
+def party(request):
+    return render(request, 'website/party.html')
+
+def directions(request):
+    return render(request, 'website/directions.html')
+
+def activities(request):
+    return render(request, 'website/activities.html')
+
+def rsvp(request):
+    return render(request, 'website/rsvp.html')
+
+def registry(request):
+    return render(request, 'website/registry.html')
