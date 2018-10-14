e9dad862f779362a54637860506add178060a7d7
Alex Bailey <mralexbailey@gmail.com>
2018-10-14 18:46:45 -0400

    More CSS and jQuery page loading

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,7 +1,43 @@
 console.log('loaded JS')
 
-
-$('li').hover(function () { 
-    console.log('Hvered');
-    MotionUI.animateIn(this, 'fade-in');
+$(document).ready(function() {
+    console.log('Doc Ready')
+    console.log(sessionStorage.visited)
+    if (!sessionStorage.visited) {
+        console.log('Visited False')
+        sessionStorage.setItem('visited', true)
+        MotionUI.animateIn('.content-holder', 'fade-in');
+        MotionUI.animateIn('ul', 'hinge-in');
+        $('.content').addClass('delayed-fade')
+        $.get('/party', function(data) {
+            $('.content').html(data)
+            MotionUI.animateIn('.content', 'fade-in');
+        })
+    } else {
+        $('.content').addClass('instant-fade')
+    };
+    // var $animation = $('.content').data('#animation')
+    // MotionUI.animateIn($('.content .delayed-fade'), 'fade-in')
+    // MotionUI.animateIn('.content', 'fade-in');
+    
+    $('a').click(function (){
+        console.log($(this).prop("name"))
+        var $page = $(this).prop("name")
+        $.get($page, function(data) {
+            $('.content').removeClass('delayed-fade')
+            $('.content').addClass('instant-fade')
+            $('.content').hide()
+            $('.content').html(data)
+            MotionUI.animateIn('.content', 'fade-in')
+            console.log(data)
+        });
+        // $('.content').html(jQuery.get($page))
+        // console.log(jQuery.get($page))
+        return false;
+    })
 });
+
+// $('li').hover(function () { 
+//     console.log('Hvered');
+//     MotionUI.animateIn(this, 'fade-in');
+// });
